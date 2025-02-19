de234cf34df4ec447975fb85d115384eee68abe0
TheDoctor8910 <40121064+TheDoctor8910@users.noreply.github.com>
2025-02-19 11:12:57 -0600

    Refactored object names

--- Python/tic_tac_toe.py
+++ Python/tic_tac_toe.py
@@ -1,47 +1,47 @@
-def p(b):
-    for r in b:
-        print(" | ".join(r))
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
         print("-" * 5)
 
 
-def c_w(b, p):
+def is_winner(board, player):
     for i in range(3):
-        if all(b[i][j] == p for j in range(3)) or all(b[j][i] == p for j in range(3)):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
             return True
-    if all(b[i][i] == p for i in range(3)) or all(b[i][2 - i] == p for i in range(3)):
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
         return True
     return False
 
 
-def f(b):
-    return all(c != " " for r in b for c in r)
+def has_full_board(board):
+    return all(col != " " for row in board for col in row)
 
 
-def t():
-    b = [[" " for _ in range(3)] for _ in range(3)]
-    p = ["X", "O"]
+def tic_tac_toe():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
     print("Tic-Tac-Toe Game")
-    p(b)
+    print_board(board)
     for t in range(9):
-        pl = p[t % 2]
+        player = players[t % 2]
         while 1:
             try:
-                r, c = map(int, input(f"P {pl}, row col (0-2): ").split())
-                if b[r][c] == " ":
-                    b[r][c] = pl
+                row, col = map(int, input(f"P {player}, row col (0-2): ").split())
+                if board[row][col] == " ":
+                    board[row][col] = player
                     break
                 else:
                     print("Nope. Again.")
             except:
                 print("Wrong. 0-2 pls.")
-        p(b)
-        if c_w(b, pl):
-            print(f"P {pl} wins!")
+        print_board(board)
+        if is_winner(board, player):
+            print(f"P {player} wins!")
             return
-        if f(b):
+        if has_full_board(board):
             print("Draw!")
             return
     print("Draw!")
 
 
-t()
+tic_tac_toe()
