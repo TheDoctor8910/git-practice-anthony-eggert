51b007121fd613ce0d2aed6178507d845a040473
TheDoctor8910 <40121064+TheDoctor8910@users.noreply.github.com>
2025-03-09 11:08:50 -0500

    Added tests

--- pytest_assignment/fixes.md
+++ pytest_assignment/fixes.md
@@ -1,7 +1,15 @@
 # Program Fixes
 The following is a list of identified fixes:
 
-1. divide_numbers: added if statement to raise a ZeroDivisionError if b = 0
-2. reverse_string: added type check on s to ensure the provided value is a string
-3. get_list_element: added 0 <= index check before attempting to return the list element
-4. get_list_element: changed `return "Not found"` to raise an IndexError instead
+1. divide_numbers
+    - bug: no handling division by zero
+    - added if statement to raise a ZeroDivisionError if b = 0
+2. reverse_string
+    - bug: problematic behavior if input is not a string
+    - added type check on s to ensure the provided value is a string
+3. get_list_element
+    - bug: improper boundary check for index
+    - added 0 <= index check before attempting to return the list element
+4. get_list_element
+    - bug: returns "Not found" when the index is out-of-bounds
+    - changed `return "Not found"` to raise an IndexError instead
--- pytest_assignment/test_program.py
+++ pytest_assignment/test_program.py
@@ -4,3 +4,37 @@ from program import divide_numbers, reverse_string, get_list_element
 
 def test_divide_numbers():
     assert divide_numbers(10, 3) == 3.33
+
+# Edge case: divide by zero should raise ZeroDivisionError
+def test_divide_numbers_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide_numbers(5, 0)
+
+def test_reverse_string():
+    assert reverse_string('aBcDeF') == 'fEdCbA'
+
+# Edge case: reverse_string with non-string argument should raise TypeError
+def test_reverse_string_with_int():
+    with pytest.raises(TypeError):
+        reverse_string(120)
+
+# Corner case: while strings are a list of chars, using reverse_string with a list should still raise TypeError
+def test_reverse_string_with_list():
+    with pytest.raises(TypeError):
+        reverse_string(['a', 'B', 'c', 'D', 'e', 'F'])
+
+def test_get_list_element():
+    test_list = [1, 3, 5, 7, 9]
+    assert get_list_element(test_list, 2) == test_list[2]
+
+# Corner case: using get_list_element with an index not less than the length of the list should raise IndexError
+def test_get_list_element_with_large_index():
+    test_list = [1, 3, 5, 7, 9]
+    with pytest.raises(IndexError):
+        get_list_element(test_list, 5)
+
+# Edge case: using get_list_element with a negative index should always raise IndexError
+def test_get_list_element_with_negative_index():
+    test_list = [1, 3, 5, 7, 9]
+    with pytest.raises(IndexError):
+        get_list_element(test_list, -1)
